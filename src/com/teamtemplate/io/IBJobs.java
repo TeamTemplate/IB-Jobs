5ca35db724830b4f5223172ed7d20b25afa539f1
mrsteakhouse <mrsteakhouse1@googlemail.com>
2013-09-18 18:20:24 +0200

    stuff geadded
    habe den anfagn vom blocklistener und das grundgerüst des plugins
    geschrieben. muss noch überlegen ob metadata oder hashmaps benutzt
    werden sollen und wie.

---
+++ src/com/teamtemplate/io/IBJobs.java
@@ -0,0 +1,15 @@
+package com.teamtemplate.io;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IBJobs extends JavaPlugin{
+
+	
+	public void onEnable() { 
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
---
+++ src/com/teamtemplate/io/JobList.java
@@ -0,0 +1,6 @@
+package com.teamtemplate.io;
+
+public enum JobList {
+
+	//liste der jobs aus einer xml datei oder so
+}
